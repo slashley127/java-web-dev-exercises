b1227941bd1b42b135ca00cb3793afc106803ba4
unknown <slashley127@gmail.com>
2022-08-17 20:27:17 -0500

    class 3 studio

---
+++ src/org/launchcode/java/studios/restaurantmenu/Menu.java
@@ -0,0 +1,22 @@
+package org.launchcode.java.studios.restaurantmenu;
+
+import java.util.Date;
+
+public class Menu {
+
+    private String restaurantName;
+    private String hoursOfOperation;
+
+    public static void main(String[] args){
+        MenuItem appetizer = new MenuItem("Gyoza", 4.95, "Delicious steamed pillow pockets of love", "Appetizer", false);
+
+        class Date{
+
+        }
+    }
+
+}
+
+    //
+
+
---
+++ src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.studios.restaurantmenu;
+
+public class MenuItem {
+
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private boolean newItem;
+
+    public MenuItem(String name, double price, String description, String category, boolean newItem){
+        this.name=name;
+        this.price=price;
+        this.description=description;
+        this.category=category;
+        this.newItem=newItem;
+
+    }
+    public class
+
+}
