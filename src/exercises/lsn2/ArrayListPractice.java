ade51996635d4d78e66a2705df90bab9653af824
unknown <slashley127@gmail.com>
2022-09-07 18:17:19 -0500

    changes from desktop

--- src/exercises/lsn2/ArrayListPractice.java
+++ src/exercises/lsn2/ArrayListPractice.java
@@ -1,6 +1,7 @@
 package exercises.lsn2;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
@@ -14,6 +15,14 @@ public class ArrayListPractice {
         numList.add(7);
         int answer = sumEven(numList);
         System.out.println(answer);
+
+        ArrayList<String> wordList = new ArrayList <>();
+        wordList.add("exit");
+        wordList.add("under");
+        wordList.add("marigold");
+        wordList.add("orchestra");
+        wordList.add("hotel");
+        System.out.println(wordList);
     }
 
     public static int sumEven(ArrayList<Integer> numList){
@@ -25,4 +34,15 @@ public class ArrayListPractice {
         }
         return total;
     }
+
+     public static String[] fiveLetters(ArrayList<String> wordList){
+       String[] fiveLettersArray = {};
+       for(String word : wordList){
+           if (word.length() == 5){
+
+           }
+
+       }
+        return fiveLettersArray;
+     }
 }
--- src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
+++ src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
@@ -2,20 +2,21 @@ package org.launchcode.java.studios.restaurantmenu;
 
 public class MenuItem {
 
+
     private String name;
     private double price;
     private String description;
     private String category;
-    private boolean newItem;
+    private boolean isNew;
 
     public MenuItem(String name, double price, String description, String category, boolean newItem){
         this.name=name;
         this.price=price;
         this.description=description;
         this.category=category;
-        this.newItem=newItem;
+        this.isNew=newItem;
 
     }
-    public class
+
 
 }
