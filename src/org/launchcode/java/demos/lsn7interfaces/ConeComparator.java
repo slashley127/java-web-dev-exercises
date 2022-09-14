849e08ac9bcc28a10ce47fb3fe4110f9a190bf82
unknown <slashley127@gmail.com>
2022-09-14 12:26:31 -0500

    Class 7 Exercises

---
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -0,0 +1,2 @@
+package org.launchcode.java.demos.lsn7interfaces;public class ConeComparator {
+}
---
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -0,0 +1,5 @@
+package org.launchcode.java.demos.lsn7interfaces;
+
+public class Comparator {
+    
+}
